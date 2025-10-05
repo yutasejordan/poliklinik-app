386c7dfda5ca392d705e08a8567f8455b28a8e8d
yutasejordan <yutasej@gmail.com>
2025-10-05 20:42:05 +0700

    finalize tugas 1 bengkot

---
+++ app/Models/Poli.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Poli extends Model
+{
+    protected $table = "poli";
+    protected $fillable = [
+        "nama_poli",
+        "keterangan"
+    ];
+
+    public function dokters(){
+        return $this->hasMany(User::class, 'id_poli');
+    }
+}
